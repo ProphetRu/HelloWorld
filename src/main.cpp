f453c38ccd16904287836870fda587444b2e5132
ProphetRu <155024765+ProphetRu@users.noreply.github.com>
2023-12-28 13:31:23 +0300

    fix 2 for github actions

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "lib.h"
 
-int main (int, char*) noexcept
+int main (int, char**) noexcept
 {
     std::cout << "Version: " << version() << std::endl;
     std::cout << "Hello, world!" << std::endl;
